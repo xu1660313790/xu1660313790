e1085d4f11088807b267f9c2ec0cd0824ac69d80
xu1660313790 <81515563+xu1660313790@users.noreply.github.com>
2021-03-28 23:11:42 +0800

    Update Operation.cs

--- ConsoleApplication1/Operation.cs
+++ ConsoleApplication1/Operation.cs
@@ -7,7 +7,7 @@ namespace ConsoleApplication1
 {
     class Operation
     {
-        public void Equals(int a, int b) {
+       public void Equals(int a, int b) {
             if (a == b)
                 Console.WriteLine("两数相同");
             else
@@ -25,6 +25,26 @@ namespace ConsoleApplication1
         }
         public int ChuFa(int a, int b) {
             return (a / b);
+        }
+        public string jia(string a, string b)
+        {
+            return (a + b);
+        }
+        public void jian(string a, string b) {
+            int i,j;
+            bool f,g;
+            g = true;
+            string str;
+            f = a.Contains(b);
+            if (f ==g)
+            {
+                i = a.IndexOf(b);
+                j = b.Length;
+                str = a.Remove(i, j);
+                Console.WriteLine("计算结果为：" + str);
+            }
+            else
+                Console.WriteLine("这两个字符串无法相减");
         }
     }
 }
