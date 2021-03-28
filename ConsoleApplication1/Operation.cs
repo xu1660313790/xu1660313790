f61f7dcc5a8b1a1a4a8dacfd3d90938fae24e3b9
xu1660313790 <81515563+xu1660313790@users.noreply.github.com>
2021-03-28 08:09:13 -0500

    Add files via upload

---
+++ ConsoleApplication1/Operation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Operation
+    {
+        public void Equals(int a, int b) {
+            if (a == b)
+                Console.WriteLine("两数相同");
+            else
+                Console.WriteLine("两数不相等");
+        }
+        public int JiaFa(int a, int b) {
+                return (a + b);
+        }
+    
+        public int JianFa(int a, int b) {
+            return (a - b);
+        }
+        public int ChengFa(int a, int b) {
+            return (a * b);
+        }
+        public int ChuFa(int a, int b) {
+            return (a / b);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            String n;
+            Operation p = new Operation();
+            Console.WriteLine("请输入计算的式子");
+            x = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToString(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            p.Equals(x, y);
+            try
+            {
+                if (n == "+")
+                    Console.WriteLine("计算结果为：{0}", p.JiaFa(x, y));
+                if (n == "-")
+                    Console.WriteLine("计算结果为：{0}", p.JianFa(x, y));
+                if (n == "*")
+                    Console.WriteLine("计算结果为：{0}", p.ChengFa(x, y));
+                if (n == "/")
+                    Console.WriteLine("计算结果为：{0}",p.ChuFa(x, y));
+            }
+            catch {
+                Console.WriteLine("错误：除数不能为0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
